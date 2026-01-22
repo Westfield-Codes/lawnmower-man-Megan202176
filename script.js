19cdb4935d1d851e4e8dd3241798df02e47078ef
Megan202176 <megan202176@student-schoolsofwestfield.org>
2026-01-22 17:10:03 +0000

    done

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 function setup(){
+  document.getElementById("yes").style.display = "none";
+  document.getElementById("no").style.display = "none";
+}
+
+function okay(){
+    let user=document.getElementById("user").value;
+    let pass=document.getElementById("pass").value;
+    if(user=="Lawnmower Man" && pass=="7.3942"){
+        document.getElementById("no").style.display = "none";
+      document.getElementById("box").style.display = "none";
+      document.getElementById("yes").style.display = "block";
+      
+    }
+    else{ document.getElementById("box").style.display = "block";
+       document.getElementById("no").style.display = "block";}
 }
